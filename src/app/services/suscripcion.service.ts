e1801c9ea9a312353d763349405c3d5b328112d8
Santi M  🐱‍👤🔱 <150541114+gxtti70@users.noreply.github.com>
2026-06-25 18:28:41 -0500

    Refactor SuscripcionService to use dynamic BASE_URL
    Updated apiUrl to use BASE_URL for dynamic configuration in production.

--- src/app/services/suscripcion.service.ts
+++ src/app/services/suscripcion.service.ts
@@ -2,22 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Suscripcion } from '../models/suscripcion';
+import { BASE_URL } from './api.config'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuscripcionService {
 
-  private apiUrl = 'http://localhost:8080/api/suscripciones';
+  // Ahora apunta dinámicamente 
+  private apiUrl = `${BASE_URL}/api/suscripciones`;
 
   constructor(private http: HttpClient) { }
 
-  // GET: Obtener todas las suscripciones activas
+  // GET: Obtener todas las suscripciones activas en producción
   getSuscripciones(): Observable<Suscripcion[]> {
     return this.http.get<Suscripcion[]>(this.apiUrl);
   }
 
-  // POST: Registrar una nueva suscripción
+  // POST: Registrar una nueva suscripción en producción
   crearSuscripcion(suscripcion: Suscripcion): Observable<Suscripcion> {
     return this.http.post<Suscripcion>(this.apiUrl, suscripcion);
   }
