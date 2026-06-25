8d957251d142eb53119d0abf0e4383b793dee077
Santi M  🐱‍👤🔱 <150541114+gxtti70@users.noreply.github.com>
2026-06-25 18:23:47 -0500

    Refactor CategoriaService to use dynamic API URL
    Updated apiUrl to use BASE_URL for dynamic endpoint.

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Categoria } from '../models/categoria';
+import { BASE_URL } from './api.config'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
-  // La URL exacta de tu backend en Spring Boot
-  private apiUrl = 'http://localhost:8080/api/categorias';
+  // Ahora apunta dinámicamente 
+  private apiUrl = `${BASE_URL}/api/categorias`;
 
-  // Inyectamos el HttpClient que configuramos en el paso 1
+  // Inyectamos el HttpClient por constructor
   constructor(private http: HttpClient) { }
 
-  // Método para traer todas las categorías
+  // Método para traer todas las categorías desde producción
   getCategorias(): Observable<Categoria[]> {
     return this.http.get<Categoria[]>(this.apiUrl);
   }
