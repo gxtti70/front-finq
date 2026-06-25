c4cbd85a1522af455b25634b6cf4c9c93d53417d
Santi M  🐱‍👤🔱 <150541114+gxtti70@users.noreply.github.com>
2026-06-25 18:30:58 -0500

    Refactor TransaccionService to use dynamic API URL
    Updated apiUrl to use BASE_URL for dynamic endpoint.

--- src/app/services/transaccion.ts
+++ src/app/services/transaccion.ts
@@ -2,23 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Transaccion } from '../models/transaccion';
+import { BASE_URL } from './api.config'; 
 
 @Injectable({
   providedIn: 'root'
 })
-
-export class TransaccionService { // <-- ¡Aquí está el nombre exacto que el Dashboard estaba buscando!
+export class TransaccionService {
   
-  private apiUrl = 'http://localhost:8080/api/transacciones';
+  // Ahora apunta dinámicamente 
+  private apiUrl = `${BASE_URL}/api/transacciones`;
 
   constructor(private http: HttpClient) { }
 
-  // GET: Obtener todas las transacciones (historial)
+  // GET: Obtener todas las transacciones (historial) en producción
   getTransacciones(): Observable<Transaccion[]> {
     return this.http.get<Transaccion[]>(this.apiUrl);
   }
 
-  // POST: Enviar una nueva transacción al backend
+  // POST: Enviar una nueva transacción al backend en producción
   crearTransaccion(transaccion: Transaccion): Observable<Transaccion> {
     return this.http.post<Transaccion>(this.apiUrl, transaccion);
   }
